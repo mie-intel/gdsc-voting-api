d0d560e12aa7858e321fe8503b34c9fe8ab3267e
mie-intel <paryapradhanika@gmail.com>
2023-11-15 12:40:00 +0700

    update connection 4

--- index.js
+++ index.js
@@ -2,25 +2,37 @@ const express = require("express");
 const mongoose = require("mongoose");
 const port = process.env.PORT || 3000;
 
-require("dotenv").config();
-
-const mongooseString = process.env.DATABASE_URL;
-
-mongoose.connect(mongooseString);
-const database = mongoose.connection;
-
-database.on("error", (error) => {
-  console.log(error);
-});
-
 const routes = require("./routes/routes.js");
 const app = express();
 
 app.use(express.json());
 app.use("/api", routes);
 
+require("dotenv").config();
+
+const mongooseString = process.env.DATABASE_URL;
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(mongooseString);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+const database = mongoose.connection;
+
+database.on("error", (error) => {
+  console.log(error);
+});
+
 database.once("connected", () => {
   console.log("Database connected");
+});
+
+connectDB().then(() => {
   app.listen(port, () => {
     console.log(`App run in server ${port}`);
   });
